6558bc8249c783908633c50180f39104fb3f550d
elenabaltoi <106616164+elenabaltoi@users.noreply.github.com>
2022-07-05 13:11:16 +0300

    Delete modul5 directory

--- modul5/my_module.py
+++
@@ -1,7 +0,0 @@
-def my_function():
-    print(__name__)
-    # print(type(__name__))
-
-if __name__=="__main__":
-
-    my_function()
--- modul5/pachet/__init__.py
+++
@@ -1,4 +0,0 @@
-my_var=1110
-
-from modul5.pachet.modul1 import
-from .modul1 import my_function  #daca punem punct in fata, reprezinta directorul curent
